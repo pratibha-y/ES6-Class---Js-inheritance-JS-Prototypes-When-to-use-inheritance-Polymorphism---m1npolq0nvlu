b30735c18faa69b2115bbeacd99615c28d4c882c
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-06-19 19:55:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,22 @@
-module.exports = { API }
+class API {
+	#secure;
+	url;
+	method;
 
-class API{
-    url;
-    constructor(url){
-        this.url=url;
-        this.method='GET';
-    }
-    url(){
-       return this.url; 
-    }
-    method(){
-        return this.method;
-    }
-
-    #secure(){
-        if(this.url.indexOf("https://")==0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    isSecure(){
-        if(this.#secure()){
-            return 'true';
-        }
-        else{
-            return 'false';
-        }
-    }
-
-    updateUrl(url){
-        this.url=url;
-    }
+	constructor(url) {
+		this.url = url;
+		this.method = 'GET';
+		this.#secure = url.includes('https');
+	}
+	isSecure() {
+		return this.#secure;
+	}
+	updateUrl(url) {
+		this.url = url;
+		this.#secure = url.includes('https');
+	}
 }
 
-const s=new API('https://api. com/api/hello');
-console.log(s.method);
-console.log(s.isSecure());
-s.updateUrl('http://api. com/api/hello');
-console.log(s.isSecure())
-console.log(s.secure);
+// const s = new API('http://api.com/api/hello');
+
+module.exports = { API };
