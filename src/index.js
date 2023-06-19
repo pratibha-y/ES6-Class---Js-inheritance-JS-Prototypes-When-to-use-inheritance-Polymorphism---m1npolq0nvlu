504aea0b5c9df1c0d852e247b08b11f4d54586af
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-06-19 19:25:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-//Write your code here
+*class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    #secure = url.startsWith("https") ? true : false;
+  }
+  isSecure() {
+    return this.#secure;
+  }
+  updateURL(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith("https") ? true : false;
+  }
+}
+module.exports = { API };*/
 
-
-
-
-
-//Do not remove this code
+class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 module.exports = { API }
